495a9e1b65e413a6f723d875301f212f42bae5b8
codey-doo-dev <m95730047@gmail.com>
2026-03-04 23:43:11 +0500

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import {  } from "";
+
 
 const Navbar = () => {
   return (
-    <div className="bg-gray-800 text-white p-4">
+    <nav className="bg-gray-800 text-white p-4">
       <h1 className="text-xl font-bold">My Website</h1>
-    </div>
+   
+    <div>
+    <ul>
+      <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Home</li>
+      <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">About</li>
+      <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Contact</li>
+    </ul>
+   </div>
+    </nav>
   )
 }
 
