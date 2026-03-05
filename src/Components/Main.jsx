3de46128340374961032768256fe234fe4fd9fbb
codey-doo-dev <m95730047@gmail.com>
2026-03-05 12:14:16 +0500

    4th commit

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Main = () => {
   return (
     <div className='flex flex-col bg-gray-100'>
-      
+      <h1 className='text-3xl font-bold mt-8 mb-4'>Main Section</h1>
+      <p className='text-lg text-gray-700 mb-4'>This is the main content area.This is the main content areaThis is the main content areaThis is the main content areaThis is the main content areaThis is the main content areaThis is the main content area</p>
     </div>
   )
 }
